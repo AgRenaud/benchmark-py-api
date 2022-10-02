48d2d7487ba6b3a770313d73fb930350007a51f9
AgRenaud <renaud.aguirrechambilla@gmail.com>
2022-10-02 22:23:30 +0200

    feat: add provision files

---
+++ build/install-python.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+install_deps() {
+    apt-get install -y \
+        make \
+        build-essential \
+        libssl-dev \
+        zlib1g-dev \
+        libbz2-dev \
+        libreadline-dev \
+        libsqlite3-dev \
+        wget \
+        curl \
+        llvm \
+        libncurses5-dev \
+        libncursesw5-dev \
+        xz-utils \
+        tk-dev \
+        libffi-dev \
+        liblzma-dev \
+        python-openssl \
+        git
+}
+
+install_pyenv() {
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+}
+
+install_poetry() {
+    POETRY_HOME="/etc/poetry"
+    curl -sSL https://install.python-poetry.org | POETRY_HOME=$POETRY_HOME python3 -
+
+}
+
+install_deps
+install_pyenv
