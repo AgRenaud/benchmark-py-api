b00d22790ed510ef0db02e3c0c9c8ffec98388fc
AgRenaud <renaud.aguirrechambilla@gmail.com>
2022-11-21 23:45:50 +0100

    fix: schema validation

--- flask-example/flask_example/schema.py
+++ flask-example/flask_example/schema.py
@@ -5,7 +5,13 @@ from datetime import datetime
 
 
 class ValidationError(Exception):
-    pass
+    def __init__(self, expected, actual):
+        self.expected_value = expected
+        self.received_value = actual
+        msg = f"Expected: {self.expected_value}, receive: {self.received_value}"
+
+        super().__init__(msg)
+
 
 class PetType(str, Enum):
     DOG = "DOG"
@@ -37,15 +43,18 @@ def dataclass_from_dict(klass: T, d) -> T:
         return d
 
 
-
 def validate_schema(expected_schema: T, input_dict: dict) -> T:
     input_attr = dict()
+    print(expected_schema)
     for attr, attr_type in expected_schema.__annotations__.items():
         try:
-            if hasattr(attr_type, "__dict__"):
-                input_attr[attr] = validate_schema(input_dict[attr])
+            if issubclass(attr_type, Enum):
+                print(f"receive Enum {attr_type}, {attr}")
+                input_attr[attr] = attr_type[input_dict[attr]]
+            elif hasattr(attr_type, '__annotations__'):
+                input_attr[attr] = validate_schema(attr_type, input_dict[attr])
             else:
                 input_attr[attr] = attr_type(input_dict[attr])
         except:
-            raise ValidationError
+            raise ValidationError(attr_type, input_dict[attr])
     return dataclass_from_dict(expected_schema, input_attr)
